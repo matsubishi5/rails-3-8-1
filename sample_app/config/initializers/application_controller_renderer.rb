add560f2a67d2afe95f96b6020eff4e0b02edf03
TakumiMatsuo <user@matsuotakumi-no-MacBook-Air.local>
2020-03-04 11:49:55 +0900

    [Finish]3-8

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '3cc869cd69a94851c8f3ae5aa0c22cd85d295ad318d76903ed9b38117e335d3c8015c808d413137744dead5fd71aae3223ba143ea22ba72885a9711a3d21024a'
